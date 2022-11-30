ae42a9b223c20843a0e0e1ef8de17b556924ace8
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-11-30 14:46:19 -0500

    Sección 10.4: Automate GET Request - Assert Response body

--- src/test/java/com/rest/AutomateGet.java
+++ src/test/java/com/rest/AutomateGet.java
@@ -1,4 +1,48 @@
 package com.rest;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+
 public class AutomateGet {
+
+    protected Headers headers = new Headers();
+
+    @Test
+    public void validateGetStatusCode() {
+        //Dado
+        given()
+                //Una ruta de solicitud
+                .baseUri("https://api.postman.com")
+                //Encabezado de una solicitud, en este caso es la API Key
+                //Se representan con un par clave-valor
+                .header(headers.HEADER_ACCESSKEY)
+                //Cuando
+                .when()
+                //Llamamos al endpoint (parte final de la URI para acceder)
+                .get("/workspaces")
+                //Entonces
+                .then()
+                //Devuelve la respuesta
+                .log().all()
+                //Y verificamos un codigo de estado de 200
+                .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void validateResponseBody() {
+        given()
+                .baseUri("https://api.postman.com")
+                .header(headers.HEADER_ACCESSKEY)
+                .when()
+                .get("/workspaces")
+                .then()
+                .log().all()
+                .assertThat().statusCode(200)
+                //Verificamos que el JSON en la ubicación dada contenga una coleccion de elementos
+                .body("workspaces.name", hasItems("Curso Udemy", "Curso de Postman GeekQA", "Curso Rest Assured API"),
+                        //Se pueden hacer varias validaciones al tiempo
+                        "workspaces.type", hasItems("personal"));
+    }
 }
