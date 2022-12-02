2a7c48b2dfa838a05240119a5f1add92c5544ac3
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-02 10:11:59 -0500

    Sección 13.2: Reuse Request Specification

--- src/test/java/com/rest/RequestSpecificationExample.java
+++ src/test/java/com/rest/RequestSpecificationExample.java
@@ -1,13 +1,24 @@
 package com.rest;
 
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class RequestSpecificationExample {
 
+    //Creamos una variable de especificaciones de requerimientos
+    RequestSpecification requestSpecification;
     protected ProjectHeaders projectHeaders = new ProjectHeaders();
+
+    //Creamos un metodo con las especificaciones
+    @BeforeClass
+    public void beforeClass() {
+        requestSpecification = given()
+                .baseUri("https://api.postman.com")
+                .header(projectHeaders.HEADER_ACCESSKEY);
+    }
 
     @Test
     public void validateStatusCode() {
@@ -16,7 +27,18 @@ public class RequestSpecificationExample {
                 .baseUri("https://api.postman.com")
                 .header(projectHeaders.HEADER_ACCESSKEY);
 
+        given()
+                //Llamamos las especificaciones para la solicitud
+                .spec(requestSpecification)
+                .when()
+                .get("/workspaces")
+                .then()
+                .log().all()
+                .assertThat().statusCode(200);
+    }
 
+    @Test
+    public void validateResponseBody() {
         given()
                 //Llamamos las especificaciones para la solicitud
                 .spec(requestSpecification)
