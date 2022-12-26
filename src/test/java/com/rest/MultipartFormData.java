c5ba979c9b8172fb72cb85ecdf1885afd78252c1
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-26 11:08:49 -0500

    Sección 20.1: Multipart Form Data - Upload File

--- src/test/java/com/rest/MultipartFormData.java
+++ src/test/java/com/rest/MultipartFormData.java
@@ -2,6 +2,8 @@ package com.rest;
 
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 
 public class MultipartFormData {
@@ -12,6 +14,26 @@ public class MultipartFormData {
         given()
                 .baseUri("https://postman-echo.com/")
                 .multiPart("foo1", "bar1")
+                .log().all()
+                .when()
+                .post("/post")
+                .then()
+                .log().all()
+                .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void uploadFileMultipartFormData() {
+        //Creamos un objeto tipo 'String' si vamos a enviar el body clave-valor con un json
+        String attributes = "{\"name\":\"temp.txt\",\"parent\":{\"id\":\"123456\"}}";
+
+        given()
+                .baseUri("https://postman-echo.com/")
+                //Si vamos a enviar el body con un archivo
+                //Ponemos el nombre del objeto tipo 'File' a crear y luego la ruta del archivo
+                .multiPart("file", new File("target/temp.txt"))
+                //Enviamos los datos con la clave y valor tipo json, se debe especificar el tipo del objeto
+                .multiPart("attributes", attributes, "application/json")
                 .log().all()
                 .when()
                 .post("/post")
