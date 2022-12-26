33ed5b956a28749b5bd742c75d65361981a9e6e3
Daniela Aguilar Ceballos <daaguilar@sistecredito.com>
2022-12-26 10:16:55 -0500

    Sección 18.3: Multiple Query Parameters

--- src/test/java/com/rest/RequestParameters.java
+++ src/test/java/com/rest/RequestParameters.java
@@ -2,6 +2,8 @@ package com.rest;
 
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
 
 public class RequestParameters {
@@ -13,6 +15,25 @@ public class RequestParameters {
                 .baseUri("https://postman-echo.com")
                 //Se puede usar 'param' o 'queryParam'
                 .param("foo1", "bar1")
+                .log().all()
+                .when()
+                .get("/get")
+                .then()
+                .log().all()
+                .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void multipleQueryParameters() {
+        //Creamos un 'HashMap' con los pares de los parametros
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("foo1", "bar1");
+        queryParams.put("foo2", "bar2");
+
+        given()
+                .baseUri("https://postman-echo.com")
+                //Se puede usar 'params' o 'queryParams'
+                .queryParams(queryParams)
                 .log().all()
                 .when()
                 .get("/get")
